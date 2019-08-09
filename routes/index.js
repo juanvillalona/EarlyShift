99ac824915f56c6eb3faeaff1f4bd4ed47000485
Juan Villalona <juan.villalona@lc.cuny.edu>
2019-08-09 15:30:47 -0400

    Fixed validator code in index.js

--- routes/index.js
+++ routes/index.js
@@ -5,33 +5,36 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
-  console.log(req.session.errors + " THIS IS REQ SESSION ERRORS");
+  //console.log(req.session.errors + " THIS IS REQ SESSION ERRORS");
 
   req.session.errors = null;
 });
 
 router.post('/submit', [ 
-  // Check validity
+  // Check validity]
   check('email', 'Invalid email address').isEmail(),
-  check('password', 'Password is invalid').isLength({min: 4})
+  check('password').isLength({min: 4}).withMessage('Password is invalid'),
+  check('password').custom(( value, {req}) => {
+    if(value !== req.body.confirmPassword) {
+      throw new Error('Password confirmation is incorrect');
+    } else {
+        return true;
+    }
+  })   
 ], (req, res, next) =>{
   //error handling
-  var errors = validationResult(req);
-  console.log(errors.isEmpty + "ERROR HERE");
+  const errors = validationResult(req);
+  console.log("This is an array" + errors.mapped());
+  console.log(req.body);
   if(!errors.isEmpty()) {
-    //req.session.errors = errors;
     console.log("there is an error");
     req.session.success = false;
-    req.session.errors = errors;
+    req.session.errors = errors.mapped();
     
-    _.each(errors, function(key, value) {
-      console.log(key, value);
-  });
-
   } else {
     console.log("there is no error");
     req.session.success = true;
-    req.session.errors = errors.array({msg});
+    
   }
   res.redirect('/');
 });
